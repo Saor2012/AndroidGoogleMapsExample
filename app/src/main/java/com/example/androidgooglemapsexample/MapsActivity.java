6fb28c61bb9f5184da463d9f55aa74eeed7d9733
Saor2012 <43040988+Saor2012@users.noreply.github.com>
2020-03-17 18:21:55 +0200

    Commit

--- app/src/main/java/com/example/androidgooglemapsexample/MapsActivity.java
+++ app/src/main/java/com/example/androidgooglemapsexample/MapsActivity.java
@@ -1,12 +1,15 @@
 package com.example.androidgooglemapsexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.FontResourcesParserCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,19 +19,32 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.model.DirectionsLeg;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.DirectionsStep;
+import com.google.maps.model.EncodedPolyline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import timber.log.Timber;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 //    private LocationListener locationListener;
 //    private LocationManager locationManager;
 //    private Context appContext = getApplicationContext();
 //    private final int REQUEST_CODE = 101;
+    private static final String TAG = "MapsActivity";
     private static final LatLng Post1 = new LatLng(48.527657,35.015481);
     private static final LatLng Post2 = new LatLng(48.525476, 35.033480);
-
     private Marker mPost1;
     private Marker mPost2;
-
     private GoogleMap mMap;
+    private GeoApiContext geoApiContext = null;
 
     public MapsActivity() {}
 /*
@@ -36,14 +52,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 * Створити базу даних, що перехоплювати точки на мапі та розмалювувати їх, тобто зберігати остані значення координа.
 * Якщо до БД щось додалося, тоді відобразити їх.
 * Організувати роботу маршруту.
+* https://aqicn.org/faq/2015-09-18/map-web-service-real-time-air-quality-tile-api/ru/ додати зв'язок з показниками
 * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -70,6 +86,72 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .position(Post2)
                 .title("Post2"));
         mPost2.setTag(0);
+
+//        String url = getURL(mPost1.getPosition(), mPost2.getPosition(), "driving");
+
+        //Define list to get all latlng for the route
+        List<LatLng> path = new ArrayList();
+
+
+        //Execute Directions API request
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey("YOUR_API_KEY")
+                .build();
+        DirectionsApiRequest req = DirectionsApi.getDirections(context, "41.385064,2.173403", "40.416775,-3.70379");
+        try {
+            DirectionsResult res = req.await();
+
+            //Loop through legs and steps to get encoded polylines of each step
+            if (res.routes != null && res.routes.length > 0) {
+                DirectionsRoute route = res.routes[0];
+
+                if (route.legs !=null) {
+                    for(int i=0; i<route.legs.length; i++) {
+                        DirectionsLeg leg = route.legs[i];
+                        if (leg.steps != null) {
+                            for (int j=0; j<leg.steps.length;j++){
+                                DirectionsStep step = leg.steps[j];
+                                if (step.steps != null && step.steps.length >0) {
+                                    for (int k=0; k<step.steps.length;k++){
+                                        DirectionsStep step1 = step.steps[k];
+                                        EncodedPolyline points1 = step1.polyline;
+                                        if (points1 != null) {
+                                            //Decode polyline and add points to list of route coordinates
+                                            List<com.google.maps.model.LatLng> coords1 = points1.decodePath();
+                                            for (com.google.maps.model.LatLng coord1 : coords1) {
+                                                path.add(new LatLng(coord1.lat, coord1.lng));
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    EncodedPolyline points = step.polyline;
+                                    if (points != null) {
+                                        //Decode polyline and add points to list of route coordinates
+                                        List<com.google.maps.model.LatLng> coords = points.decodePath();
+                                        for (com.google.maps.model.LatLng coord : coords) {
+                                            path.add(new LatLng(coord.lat, coord.lng));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            Log.e(TAG, ex.getLocalizedMessage());
+        }
+
+        //Draw the polyline
+        if (path.size() > 0) {
+            PolylineOptions opts = new PolylineOptions().addAll(path).color(Color.BLUE).width(5);
+            mMap.addPolyline(opts);
+        }
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
+
         // Add a marker in Sydney and move the camera
 //        LatLng sydney = new LatLng(-34, 151);
 //        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
@@ -97,6 +179,21 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // marker is centered and for the marker's info window to open, if it has one).
         return false;
     }
+
+    private void  calculateDirections(Marker marker) {
+        Timber
+    }
+
+//    private String getURL(LatLng start, LatLng end, String directionMode) {
+//        String string1 = "origin" + start.latitude + "," + start.longitude;
+//        String string2 = "destination" + end.latitude + "," + end.longitude;
+//        String mode = "mode=" + directionMode;
+//        String parametes = string1 + "&" + string2 + "&" + mode;
+//        String output = "json";
+//        return "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parametes + "&key=" + getString(R.string.google_maps_key);
+//    }
+
+//    @Override
 //    public void onMapReady(GoogleMap googleMap) {
 //        mMap = googleMap;
 //
@@ -171,5 +268,5 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 //
 //        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zaragoza, 6));
 //    }
-//}
+
 }
